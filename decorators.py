836e3bca954c88c7b31a1aef24a215a39687f8a5
William Hargrove <wh@datagration.co.uk>
2019-02-05 11:48:58 +0000

    added wrap function

--- decorators.py
+++ decorators.py
@@ -1,3 +1,13 @@
+def new_decorator(original_function):
+
+    def wrap_function():
+        print('Some extra code, before the original function')
+        original_function() # note we execute the original function
+        print('Some extra code, after the original function')
+
+    # notice that we are returning a function
+    return wrap_function
+
 @new_decorator
 # specifying @new_decorator above will pass function func_needs_decorator to the new_decorator function
 def func_needs_decorator():
